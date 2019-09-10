794fc5f8a916f234078a9e6d5091e1e46666e0e1
SeasSystem <jxae093663@163.com>
2019-09-10 08:46:03 +0800

    'react-HOOK版本后台控制'

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import Login from './containers/login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  return <Router>
+    <Switch>
+      {/*<Route path="/" component={Login}/>*/}
+      <div>aaakjadkfj</div>
+    </Switch>
+  </Router>
 }
-
-export default App;
